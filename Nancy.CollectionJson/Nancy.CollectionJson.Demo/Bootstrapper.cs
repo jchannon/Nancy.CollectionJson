ae3fc3c8da549daa074c42dff609a05f33510cd4
Jonathan Channon <jonathan.channon@gmail.com>
2014-10-21 17:36:58 +0100

    trying to POST

--- Nancy.CollectionJson/Nancy.CollectionJson.Demo/Bootstrapper.cs
+++ Nancy.CollectionJson/Nancy.CollectionJson.Demo/Bootstrapper.cs
@@ -7,11 +7,20 @@ namespace Nancy.CollectionJson.Demo
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            StaticConfiguration.DisableErrorTraces = false;
+        }
+
         protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
         {
             base.ConfigureApplicationContainer(container);
 
             container.RegisterMultiple<ILinkGenerator>(new[]{ typeof(FriendLinkGenerator) });
+            container.Register<ICollectionJsonDocumentReader<Friend>, FriendDocumentReader>();
+            container.Register<IWriteDocument,WriteDocument>();
         }
 
         protected override void ConfigureRequestContainer(Nancy.TinyIoc.TinyIoCContainer container, NancyContext context)
--- Nancy.CollectionJson/Nancy.CollectionJson.Demo/HomeModule.cs
+++ Nancy.CollectionJson/Nancy.CollectionJson.Demo/HomeModule.cs
@@ -3,22 +3,34 @@ using Nancy.CollectionJson.Demo.Models;
 using CollectionJson;
 using System.Linq;
 using System.Collections.Generic;
+using Nancy.ModelBinding;
 
 namespace Nancy.CollectionJson.Demo
 {
     public class HomeModule : NancyModule
     {
-        IEnumerable<ILinkGenerator> thelinks;
-
-        public HomeModule(IFriendRepository repo, IEnumerable<ILinkGenerator> thelinks)
-        {
-            this.thelinks = thelinks;
-           
-            Get["/{name}"] = parameters =>
+        public HomeModule(IFriendRepository repo, ICollectionJsonDocumentReader<Friend> friendReader)
+        {           
+            Get["/search/{name}"] = parameters =>
             {
                 var friends = repo.GetAll().Where(f => f.FullName.IndexOf(parameters.name, StringComparison.OrdinalIgnoreCase) > -1);
-                Console.Write(this.thelinks.GetHashCode().ToString());
                 return friends;
+            };
+
+            Post["/"] = _ =>
+            {
+                var data = this.Bind<List<Data>>(); //Nancy doesn't bind to List<T> located 3 levels deep on an object
+                
+                //This doesn't compile as Writedocument properties are private set
+                var doc = new WriteDocument();
+                doc.Template = new Template();
+                doc.Template.Data = data;
+                   
+                var friend = friendReader.Read(doc);
+                var id = repo.Add(friend);
+                this.Context.Response.Headers.Add("Location", "http://mydomain.com/friends/" + id);
+                return HttpStatusCode.Created;
+             
             };
         }
     }
